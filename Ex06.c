29512f5ca5214d035de7a07222c8ac5bd271fb2e
CarvalhoCintia <111870425+CarvalhoCintia@users.noreply.github.com>
2022-08-30 11:29:59 -0300

    Add files via upload

---
+++ Ex06.c
@@ -0,0 +1,32 @@
+/*	6. Faça um programa para um caixa eletrônico. O programa deverá perguntar ao usuário o valor do
+saque e depois informar quantas notas de cada valor serão fornecidas. As notas disponíveis serão as de 1,
+5, 10, 50 e 100 reais. O valor mínimo é de 10 reais e o máximo de 600 reais. O programa não deve se
+preocupar com a quantidade de notas existentes na máquina.
+Exemplo 1: Para sacar a quantia de 256 reais, o programa fornece duas notas de 100, uma nota de
+50, uma nota de 5 e uma nota de 1;
+Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de
+50, quatro notas de 10, uma nota de 5 e quatro notas de 1.*/
+#include<stdio.h>
+#include<locale.h>
+int main (){
+	setlocale(LC_ALL,"");
+	printf("Saque mínimo R$10  e máximo R$600,00.\n");
+	int saque;
+	printf("Qual o valor do saque ?R$:");
+	scanf("%d",&saque);
+	printf("\n%d notas de 100",saque /100);
+	saque = saque%100;
+	printf("\n%d notas de 50",saque /50);
+	saque = saque%50;
+	printf("\n%d notas de 10",saque /10);
+	saque = saque%10;
+	printf("\n%d notas de 5",saque /5);
+	saque = saque%5;
+	printf("\n%d notas de 1",saque /1);
+	saque = saque%1;
+	
+	
+		
+	}
+
+
